cd2bf9a88d3256f415fef13fca1d9d66a812c839
megascatterbomb <dachingygang@gmail.com>
2023-06-05 13:28:52 +1200

    rearranging some stuff

--- local/src/discord/client.ts
+++ local/src/discord/client.ts
@@ -4,28 +4,29 @@ import { Client } from "discordx";
 // Entry point to start discord server. Only supported in headless mode.
 export async function startDiscordServer() {
     const client = new Client({
-        botId: "test",
         intents: [
             IntentsBitField.Flags.Guilds,
             IntentsBitField.Flags.GuildMessages,
+            IntentsBitField.Flags.MessageContent
         ],
+        botGuilds: process.env.DEV_GUILD ? [process.env.DEV_GUILD] : undefined,
     });
 
     if(!process.env.BOT_OWNER) {
         throw new Error("BOT_OWNER not specified in environment. Whoever hosts this needs to set BOT_OWNER to their discord account ID!");
     }
 
-    client.once("ready", async () => {
+    if(process.env.DISCORD_TOKEN === undefined) {
+        throw new Error("DISCORD_TOKEN not defined. Required for headless mode.");
+    }
+
+    client.on("ready", async () => {
         await client.initApplicationCommands();
     });
 
     client.on("interactionCreate", (interaction) => {
         client.executeInteraction(interaction);
     });
-
-    if(process.env.DISCORD_TOKEN === undefined) {
-        throw new Error("DISCORD_TOKEN not defined. Required for headless mode.");
-    }
 
     await client.login(process.env.DISCORD_TOKEN);
 }
