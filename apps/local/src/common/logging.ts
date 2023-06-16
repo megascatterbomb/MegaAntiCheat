67081b68b332ab9ad1cd96d80cd0bca87bcc2d00
Finn <34718806+FinnDore@users.noreply.github.com>
2023-06-16 22:15:50 +0100

    typo

--- apps/local/src/common/logging.ts
+++ apps/local/src/common/logging.ts
@@ -1,13 +1,14 @@
-import dotenv from "dotenv";
-dotenv.config({path: "./local/.env"});
+import dotenv from 'dotenv';
 
-export let verboseEnabled = process.env.VERBOSE === "true"; 
+dotenv.config({ path: './local/.env' });
+
+export let verboseEnabled = process.env.VERBOSE === 'true';
 
 export function setVerbose(verbose: boolean) {
     verboseEnabled = verbose;
 }
 
 export default function verboseLog(content: string) {
-    if(!verboseEnabled) return;
+    if (!verboseEnabled) return;
     console.log(content);
 }
